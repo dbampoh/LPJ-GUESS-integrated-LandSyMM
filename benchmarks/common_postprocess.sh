f9a52e1f01fee0679ee1f63bfb476ed3901168c7
Paul Miller <paul.miller@nateko.lu.se>
2010-09-13 13:42:03 +0000

    100913 - Paul Miller Updated europe and global benchmarks, common_postprocess.sh, and added the pristine_sites benchmark.
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/guess2008@21 70805675-57b6-444b-bb00-18ecffcb4be8

--- benchmarks/common_postprocess.sh
+++ benchmarks/common_postprocess.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-tslice lai.out -o lai500to509.txt -f 500 -t 509 -lon 1 -lat 2 -y 3
-/home/joe/work/benchmarks/gmapall/gmapall lai500to509.txt
+tslice cmass.out -o cmass1961to1990.txt -f 560 -t 589 -lon 1 -lat 2 -y 3
+tslice lai.out -o lai1961to1990.txt -f 560 -t 589 -lon 1 -lat 2 -y 3
+tslice dens.out -o dens1961to1990.txt -f 560 -t 589 -lon 1 -lat 2 -y 3
+tslice anpp.out -o anpp1961to1990.txt -f 560 -t 589 -lon 1 -lat 2 -y 3
+tslice cflux.out -o cflux1961to1990.txt -f 560 -t 589 -lon 1 -lat 2 -y 3
+tslice cpool.out -o cpool1961to1990.txt -f 560 -t 589 -lon 1 -lat 2 -y 3
+tslice firert.out -o firert1961to1990.txt -f 560 -t 589 -lon 1 -lat 2 -y 3
+/home/joe/work/benchmarks/gmapall/gmapall lai1961to1990.txt
+/home/joe/work/benchmarks/dominance/dominance lai1961to1990.txt lai1961to1990max.txt
