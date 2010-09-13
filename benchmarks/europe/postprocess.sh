4da23ef823fca0c0159701fb09f388182ee49105
Paul Miller <paul.miller@nateko.lu.se>
2010-09-13 13:59:56 +0000

    100913 - Paul Miller Added files to SVN that I had forgotten in the previous commit
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/guess2008@22 70805675-57b6-444b-bb00-18ecffcb4be8

---
+++ benchmarks/europe/postprocess.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gmap lai1961to1990max.txt -lon 1 -lat 2 -i 3 -legend legend_europe.txt -o maxLAIeurope.jpg
---
+++ benchmarks/pristine_sites/postprocess.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/home/joe/work/benchmarks/dominance/dominance cmass1961to1990.txt cmass1961to1990max.txt
+/home/joe/work/benchmarks/dominance/dominance dens1961to1990.txt dens1961to1990max.txt
