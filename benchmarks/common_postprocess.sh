406b8745df7cffd9fc17c6662556fbe89c58b2a5
Joe Siltenberg <joe.a.lindstrom@gmail.com>
2010-09-30 10:46:40 +0000

    Removed some paths to utilities used by the benchmarks.
    gmapall, dominance and bbox is now assumed to be available in the PATH.
    
    
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/guess2008@49 70805675-57b6-444b-bb00-18ecffcb4be8

--- benchmarks/common_postprocess.sh
+++ benchmarks/common_postprocess.sh
@@ -6,5 +6,5 @@ tslice anpp.out -o anpp1961to1990.txt -f 560 -t 589 -lon 1 -lat 2 -y 3
 tslice cflux.out -o cflux1961to1990.txt -f 560 -t 589 -lon 1 -lat 2 -y 3
 tslice cpool.out -o cpool1961to1990.txt -f 560 -t 589 -lon 1 -lat 2 -y 3
 tslice firert.out -o firert1961to1990.txt -f 560 -t 589 -lon 1 -lat 2 -y 3
-/home/joe/work/benchmarks/gmapall/gmapall lai1961to1990.txt
-/home/joe/work/benchmarks/dominance/dominance lai1961to1990.txt lai1961to1990max.txt
+gmapall lai1961to1990.txt
+dominance lai1961to1990.txt lai1961to1990max.txt
