bc6f4880dc47617f0949a60c5d609a814fd2b02a
Joe Siltenberg <joe.a.lindstrom@gmail.com>
2010-10-25 12:49:43 +0000

    Fixed the post processing for pristine sites slightly.
    Shouldn't use absolute paths, and dominance for cmass already runs in common1961to1990.sh.
    
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/guess2008@87 70805675-57b6-444b-bb00-18ecffcb4be8

--- benchmarks/pristine_sites/postprocess.sh
+++ benchmarks/pristine_sites/postprocess.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 common1961to1990.sh
-/home/joe/work/benchmarks/dominance/dominance cmass1961to1990.txt cmass1961to1990max.txt
-/home/joe/work/benchmarks/dominance/dominance dens1961to1990.txt dens1961to1990max.txt
+dominance dens1961to1990.txt dens1961to1990max.txt
