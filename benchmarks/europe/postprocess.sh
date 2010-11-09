acc2a8a17d8850c46a7ec3f395310d7a2aeba202
Paul Miller <paul.miller@nateko.lu.se>
2010-11-09 13:00:52 +0000

    Added titles to the maxLAI and maxCMASS maps created in postprocess.sh
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/guess2008@108 70805675-57b6-444b-bb00-18ecffcb4be8

--- benchmarks/europe/postprocess.sh
+++ benchmarks/europe/postprocess.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 common1961to1990.sh
-gmap lai1961to1990max.txt -lon 1 -lat 2 -i 3 -legend legend_europe.txt -o maxLAIeurope.jpg
+gmap lai1961to1990max.txt -t 'Dominant Species (greatest LAI)' -lon 1 -lat 2 -i 3 -legend legend_europe.txt -o maxLAIeurope.jpg
+gmap cmass1961to1990max.txt -t 'Dominant Species (greatest cmass)' -lon 1 -lat 2 -i 3 -legend legend_europe.txt -o maxCMASSeurope.jpg
--- benchmarks/global/postprocess.sh
+++ benchmarks/global/postprocess.sh
@@ -5,4 +5,4 @@ aslice cflux1961to1990.txt -o cflux1961to1990_areaaverage.txt -lon 1 -lat 2 -n -
 aslice cflux1990to2000.txt -o cflux1990to2000_areaaverage.txt -lon 1 -lat 2 -n -pixsize 0.5 0.5 -pixoffset 0.25 0.25   
 aslice cpool1961to1990.txt -o cpool1961to1990_areaaverage.txt -lon 1 -lat 2 -n -pixsize 0.5 0.5 -pixoffset 0.25 0.25   
 aslice tot_runoff1961to1990.txt -o tot_runoff1961to1990_areaaverage.txt -lon 1 -lat 2 -n -pixsize 0.5 0.5 -pixoffset 0.25 0.25   
-gmap lai1961to1990max.txt -lon 1 -lat 2 -i 3 -legend legend_global.txt -o maxLAI.jpg
+gmap lai1961to1990max.txt -t 'Dominant PFT (greatest LAI)' -lon 1 -lat 2 -i 3 -legend legend_global.txt -o maxLAI.jpg
