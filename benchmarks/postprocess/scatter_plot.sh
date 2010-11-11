9d99733c148c72c11b319339affe8fb019e4b04f
Joe Siltenberg <joe.a.lindstrom@gmail.com>
2010-11-11 13:25:41 +0000

    Modified the scatter plots for EMDI and Euroflux benchmarks.
    Now includes rms of residuals and the function y=x. Also added a legend to
    describe the plotted data.
    
    
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/guess2008@110 70805675-57b6-444b-bb00-18ecffcb4be8

--- benchmarks/postprocess/scatter_plot.sh
+++ benchmarks/postprocess/scatter_plot.sh
@@ -14,18 +14,34 @@ function scatter_plot {
     local image=$5
 
 # The gnuplot script
+#
+# To get the rms of residuals, gnuplot is run first once just to do a regression,
+# the rms will end up in the file fit.log, which is then parsed in the second
+# run of gnuplot below.
+    cat <<EOF | gnuplot &> /dev/null
+y(x) = m*x+c
+fit y(x) "${datafile}" via m,c
+EOF
+
+# This is where the actual plotting is done
     cat <<EOF | gnuplot &> /dev/null
 set title "${title}"
 set xlabel "${xtitle}"
 set ylabel "${ytitle}"
-set key off
+set key on
 set grid ytics
 set terminal png
 set output "${image}"
 set fit logfile "/dev/null"
 y(x) = m*x+c
+id(x) = x
 fit y(x) "${datafile}" via m,c
+rms = `cat fit.log | grep "^rms of residuals.*\:.*" | awk '{ print $NF }'`
 set label 'y = %.3f',m,'x + %.3f',c at screen 0.15,0.85 front
-plot "${datafile}" with points lt 3 pt 5, y(x)
+set label 'rms of residuals = %.3f', rms at screen 0.15,0.80 front
+plot "${datafile}" title "data point" with points lt 3 pt 5, \
+     y(x) title "linear regression", \
+     id(x) title "y = x"
 EOF
+    rm fit.log
 }
