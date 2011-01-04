8432f0ff76a6faea40c643046d6b41cb8d724806
Joe Siltenberg <joe.a.lindstrom@gmail.com>
2011-01-04 09:20:44 +0000

    Bug fix: the descriptions in the gridlist weren't read in correctly.
    If there were spaces in the description for a grid, only the first word would
    be read in as the description. Now the whole rest of the line is read in as
    description.
    
    
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/tech@172 70805675-57b6-444b-bb00-18ecffcb4be8

--- benchmarks/euroflux/source/cru/guessio/guessio_cru.cpp
+++ benchmarks/euroflux/source/cru/guessio/guessio_cru.cpp
@@ -1748,7 +1748,7 @@ void initio(int argc,char* argv[],Pftlist& pftlist) {
 	while (!eof) {
 		
 		// Read next record in file
-		//eof=!readfor(in_grid,"f,f,a",&dlon,&dlat,&descrip);
+		//eof=!readfor(in_grid,"f,f,a#",&dlon,&dlat,&descrip);
 
 		// New, local versions of these arrays
 		xtring dom_spec[5];
