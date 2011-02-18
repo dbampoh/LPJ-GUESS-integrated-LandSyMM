f61f6adcedf2618e49e0e10e250b267e9d94b124
Joe Siltenberg <joe.a.lindstrom@gmail.com>
2011-02-18 15:00:30 +0000

    Disabled a few specific warnings for Visual Studio and fixed a few others.
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/tech@296 70805675-57b6-444b-bb00-18ecffcb4be8

--- framework/guess.cpp
+++ framework/guess.cpp
@@ -71,8 +71,8 @@ Stand::Stand(int i, Gridcell& gc,landcovertype landcoverX,Pftlist& pftlist):id(i
 		// Constructor: initialises reference member of climate and
 		// builds list array of Standpft objects
 		
-	int p;
-	int npatchL;
+	unsigned int p;
+	unsigned int npatchL;
 
 	for(p=0;p<pftlist.nobj;p++) {
 		pft.createobj(pftlist[p]);
@@ -157,7 +157,6 @@ Individual::Individual(int i,Pft& p,Vegetation& v):id(i),pft(p),vegetation(v) {
 int framework(int argc,char* argv[]) {
 
 	bool dogridcell;
-	int p;
 
 	// The one and only linked list of Pft objects	
 	Pftlist pftlist;
