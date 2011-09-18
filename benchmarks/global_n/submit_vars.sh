e64e439a087fc95c7c62cc53778e2bbea38ba3db
David Wårlind <david.warlind@nateko.lu.se>
2011-09-18 00:10:47 +0000

    bug fixing
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/nitrogen@746 70805675-57b6-444b-bb00-18ecffcb4be8

--- benchmarks/global_n/submit_vars.sh
+++ benchmarks/global_n/submit_vars.sh
@@ -1,2 +1,2 @@
-OUTFILES=${OUTFILES}' anppn.out cton.out nmass.out npool.out nsources.out nleach.out nuptake.out vmaxnlim.out '
+OUTFILES=${OUTFILES}' anppn.out cton.out nmass.out npool.out nsources.out nleach.out nuptake.out vmaxnlim.out'
 NPROCESS=10
