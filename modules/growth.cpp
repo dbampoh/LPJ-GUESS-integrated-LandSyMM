011b5a36973aaab31aee67f621baa0548bbb9710
David Wårlind <david.warlind@nateko.lu.se>
2012-11-21 15:55:00 +0000

    Fixed a bug in simba.tmpl for serialization
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/nitrogen@2010 70805675-57b6-444b-bb00-18ecffcb4be8

--- modules/growth.cpp
+++ modules/growth.cpp
@@ -184,7 +184,7 @@ void turnover(double turnover_leaf, double turnover_root, double turnover_sap,
 	double& cmass_heart, double& nmass_leaf, double& nmass_root, double& nmass_sap,
 	double& nmass_heart, double& litter_leaf, double& litter_root,
 	double& nmass_litter_leaf, double& nmass_litter_root,
-	double& retransn, Fluxes& fluxes, bool alive, double& nmass,
+	double& retransn, bool alive, double& nmass,
 	landcovertype landcover, Gridcell& gridcell) {
 
 	// DESCRIPTION
@@ -1149,7 +1149,7 @@ void growth(Stand& stand, Patch& patch) {
 				patch.pft[indiv.pft.id].nmass_litter_leaf,
 				patch.pft[indiv.pft.id].nmass_litter_root,
 				retransn, 
-				patch.fluxes, indiv.alive, patch.soil.nmass,
+				indiv.alive, patch.soil.nmass,
 				indiv.pft.landcover, gridcell);
 
 			if (indiv.alive && date.year > freenyears && indiv.nstore + retransn > indiv.max_n_storage) {
