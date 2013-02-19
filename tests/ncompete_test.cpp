2e1483c56166e9fb8a7c18662d10894389b69b84
Joe Siltenberg <joe.a.lindstrom@gmail.com>
2013-02-19 08:39:00 +0000

    Added a few tests for ncompete()
    The last test failes, presumably due to a bug in ncompete.
    
    
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/nitrogen@2334 70805675-57b6-444b-bb00-18ecffcb4be8

---
+++ tests/ncompete_test.cpp
@@ -0,0 +1,71 @@
+///////////////////////////////////////////////////////////////////////////////////////
+/// \file ncompete_test.cpp
+/// \brief Unit tests for nitrogen uptake competition
+///
+/// \author Joe Lindström
+/// $Date$
+///
+///////////////////////////////////////////////////////////////////////////////////////
+
+#include "catch.hpp"
+
+#include "ncompete.h"
+
+TEST_CASE("ncompete/single", "Testing a single individual") {
+	std::vector<NCompetingIndividual> indivs(1);
+
+	indivs[0].ndemand = 1;
+	indivs[0].strength = 1;
+	indivs[0].isgrass = false;
+
+	// more N available than needed
+	ncompete(indivs, 2.0, 2.0);
+
+	REQUIRE(indivs[0].fnuptake == Approx(1));
+
+
+	// less N available than needed
+	ncompete(indivs, 0.5, 0.5);
+
+	REQUIRE(indivs[0].fnuptake == Approx(0.5));
+
+	// change to grass
+
+	indivs[0].isgrass = true;
+
+	// more N available than needed
+	ncompete(indivs, 2.0, 2.0);
+
+	REQUIRE(indivs[0].fnuptake == Approx(1));
+
+
+	// less N available than needed
+	ncompete(indivs, 0.5, 0.5);
+
+	REQUIRE(indivs[0].fnuptake == Approx(0.5));	
+}
+
+
+TEST_CASE("ncompete/double", "Testing two individuals") {
+	std::vector<NCompetingIndividual> indivs(2);
+
+	// two equal individuals
+
+	indivs[0].ndemand = 1;
+	indivs[0].strength = 1;
+	indivs[0].isgrass = false;
+	
+	indivs[1] = indivs[0];
+
+	// more N available than needed
+	ncompete(indivs, 3.0, 3.0);
+	
+	REQUIRE(indivs[0].fnuptake == Approx(1));
+	REQUIRE(indivs[1].fnuptake == Approx(1));
+
+	// less N available than needed
+	ncompete(indivs, 1.0, 1.0);
+
+	REQUIRE(indivs[0].fnuptake == Approx(0.5));
+	REQUIRE(indivs[1].fnuptake == Approx(0.5));
+}
