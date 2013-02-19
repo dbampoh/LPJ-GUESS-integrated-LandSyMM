aede9b9140e3240f4c1ee587e41003061b28424f
Joe Siltenberg <joe.a.lindstrom@gmail.com>
2013-02-19 10:23:55 +0000

    A few more simple tests of ncompete()
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/nitrogen@2336 70805675-57b6-444b-bb00-18ecffcb4be8

--- tests/ncompete_test.cpp
+++ tests/ncompete_test.cpp
@@ -68,4 +68,39 @@ TEST_CASE("ncompete/double", "Testing two individuals") {
 
 	REQUIRE(indivs[0].fnuptake == Approx(0.5));
 	REQUIRE(indivs[1].fnuptake == Approx(0.5));
+
+	// change to grass
+
+	indivs[0].isgrass = true;
+	indivs[1].isgrass = true;
+
+	// more N available than needed
+	ncompete(indivs, 3.0, 3.0/2.0);
+	
+	REQUIRE(indivs[0].fnuptake == Approx(1));
+	REQUIRE(indivs[1].fnuptake == Approx(1));
+
+	// less N available than needed
+	ncompete(indivs, 1.0, 1.0/2.0);
+
+	REQUIRE(indivs[0].fnuptake == Approx(0.5));
+	REQUIRE(indivs[1].fnuptake == Approx(0.5));
+
+	// make the second indiv twice as strong
+	indivs[1].strength *= 2;
+
+	// less N available than needed
+	ncompete(indivs, 1.0, 1.0/2.0);
+
+	REQUIRE(indivs[0].fnuptake == Approx(1.0/3.0));
+	REQUIRE(indivs[1].fnuptake == Approx(2.0/3.0));
+
+	// reduce the demand for the stronger individual
+	indivs[1].ndemand = 0.1;
+
+	// less N available than needed
+	ncompete(indivs, 1.0, 1.0/1.1);
+
+	REQUIRE(indivs[0].fnuptake == Approx(0.9));
+	REQUIRE(indivs[1].fnuptake == Approx(1));
 }
