7cb2cd9d633a73f895d14091edff1eabacc24abf
Joe Siltenberg <joe.a.lindstrom@gmail.com>
2013-02-25 09:55:29 +0000

    Minor bugfix in soilwater.cpp to avoid negative wcont_evap
    This was done in RCA-GUESS first, see r2359 for more info.
    
    
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/bugfix@2361 70805675-57b6-444b-bb00-18ecffcb4be8

--- modules/soilwater.cpp
+++ modules/soilwater.cpp
@@ -175,6 +175,10 @@ void hydrology_lpjf(Patch& patch, Climate& climate, double rain_melt, double per
 	wcont_evap += (rain_melt-aet_layer[0]*SOILDEPTH_EVAP*K_AET_DEPTH/SOILDEPTH_UPPER-evap)
 		/awc[0];
 
+	if (wcont_evap < 0) {
+		 wcont_evap = 0;
+	}
+
 	if (wcont_evap > wcont[0]) {
 		wcont_evap = wcont[0];
 	}
