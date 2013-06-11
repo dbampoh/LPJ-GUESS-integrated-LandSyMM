dcb5f4ab659a5fc4107e231a0aca5151bfe25c36
Mats Lindeskog <mats.lindeskog@nateko.lu.se>
2013-06-11 10:32:06 +0000

    Fix in establishment to prepare for managed forest update.
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/crop@2652 70805675-57b6-444b-bb00-18ecffcb4be8

--- modules/vegdynam.cpp
+++ modules/vegdynam.cpp
@@ -418,7 +418,7 @@ void establishment_guess(Stand& stand,Patch& patch) {
 	pftlist.firstobj();
 	while (pftlist.isobj) {
 		Pft& pft=pftlist.getobj();
-		if (establish(patch,stand.gridcell.climate,pft) && pft.lifeform==TREE)
+		if (establish(patch,stand.gridcell.climate,pft) && pft.lifeform==TREE && stand.landcover==pft.landcover)
 			nwoodypfts_estab++;
 		pftlist.nextobj();
 	}
