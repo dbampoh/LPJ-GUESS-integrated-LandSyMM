772fb3df59d30b8e37f21d08bba3763f54e096dd
Joe Siltenberg <joe.a.lindstrom@gmail.com>
2013-10-25 12:40:42 +0000

    The new interpolation methods (conserving means) are now used.
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/nitrogen@2998 70805675-57b6-444b-bb00-18ecffcb4be8

--- cru/guessio/guessio_cru.cpp
+++ cru/guessio/guessio_cru.cpp
@@ -68,10 +68,10 @@ namespace {
 /// Interpolates monthly data to quasi-daily values.
 void interp_climate(double mtemp[12], double mprec[12], double msun[12], double mdtr[12],
 					double dtemp[365], double dprec[365], double dsun[365], double ddtr[365]) {
-	interp_monthly_means(mtemp, dtemp);
-	interp_monthly_totals(mprec, dprec);
-	interp_monthly_means(msun, dsun);
-	interp_monthly_means(mdtr, ddtr);
+	interp_monthly_means_conserve(mtemp, dtemp);
+	interp_monthly_totals_conserve(mprec, dprec, 0);
+	interp_monthly_means_conserve(msun, dsun, 0, 100);
+	interp_monthly_means_conserve(mdtr, ddtr, 0);
 }
 
 } // namespace
