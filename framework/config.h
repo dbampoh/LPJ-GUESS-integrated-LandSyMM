72e94d73b1d099e834fb64cda5bfb0bf2670e1d7
Mats Lindeskog <mats.lindeskog@nateko.lu.se>
2015-03-26 12:43:39 +0000

    Put back #define DYNAMIC_LANDCOVER_INPUT. Needed in parameters.cpp
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/crop@3998 70805675-57b6-444b-bb00-18ecffcb4be8

--- framework/config.h
+++ framework/config.h
@@ -21,6 +21,7 @@
 #define LPJ_GUESS_CONFIG_H
 
 // Defines for landcover version:
+#define DYNAMIC_LANDCOVER_INPUT		// Reads landcover data from text files, using the TimeDataD class.
 #define NEWSOWINGDATE				// Use sowing date method based on climate seasonality (modified version of Waha et al. 2012), as opposed to old method used in Bondeau et al. 2007.
 #define IRRIGATION					// Crop irrigation on
 #define NOPASTURESTOCH				// Undefine for fire and disturbance for pasture grass. Number of patches will be the same as for natural stands.
