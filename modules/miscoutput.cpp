6ed1b620ae99e523a707a44dea9006c76d28533d
Mats Lindeskog <mats.lindeskog@nateko.lu.se>
2015-11-05 21:02:52 +0000

    Correction of last commit.
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/crop_reintegration_cont@4569 70805675-57b6-444b-bb00-18ecffcb4be8

--- modules/miscoutput.cpp
+++ modules/miscoutput.cpp
@@ -535,7 +535,7 @@ void MiscOutput::outannual(Gridcell& gridcell) {
 			if (printseparatestands) {
 
 				int id = stand.id;
-				if(stand.id > MAXNUMBER_STANDS)
+				if(stand.id >= MAXNUMBER_STANDS)
 					fail("Number of stands to high, increase MAXNUMBER_STANDS for output of individual stands !\n");
 
 				if (stand.landcover == NATURAL) {
