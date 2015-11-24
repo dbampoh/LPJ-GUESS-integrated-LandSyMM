0118719ff905ac700cef3c7e1436f9ba31d24c64
Johan Nord <johan.nord@nateko.lu.se>
2015-11-24 12:36:28 +0000

    Reintroducing pixoffset in global benchmark but with offset 0.0 instead of 0.25 as adaptation to center-gridecell coordinates.
    Reintroducing those -pixoffset that were removed in r4253 tech branch (but then offset 0.25, now 0.0).
    
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/cru_ncep@4628 70805675-57b6-444b-bb00-18ecffcb4be8

--- benchmarks/global/postprocess.sh
+++ benchmarks/global/postprocess.sh
@@ -14,24 +14,24 @@ gmapall cton_leaf1961to1990.txt -P cton_leaf_ -legend common/legend_cton.txt -po
 describe_images "Leaf C:N Ratio For All PFTs (1961-90 average). Units: kgC kgN-1" cton_leaf_*.jpg
 
 tslice cflux.out -o cflux1990to2000.txt -f 1990 -t 2000 -lon 1 -lat 2 -y 3
-aslice cflux1961to1990.txt -o cflux1961to1990_areaaverage.txt -lon 1 -lat 2 -n -pixsize 0.5 0.5
+aslice cflux1961to1990.txt -o cflux1961to1990_areaaverage.txt -lon 1 -lat 2 -n -pixsize 0.5 0.5 -pixoffset 0.0 0.0
 describe_textfile cflux1961to1990_areaaverage.txt "Global Terrestrial Carbon Fluxes, 1961 to 1990. Units: kgC m-2"
-aslice cflux1990to2000.txt -o cflux1990to2000_areaaverage.txt -lon 1 -lat 2 -n -pixsize 0.5 0.5
+aslice cflux1990to2000.txt -o cflux1990to2000_areaaverage.txt -lon 1 -lat 2 -n -pixsize 0.5 0.5 -pixoffset 0.0 0.0
 describe_textfile cflux1990to2000_areaaverage.txt "Global Terrestrial Carbon Fluxes, 1990 to 2000. Units: kgC m-2"
 
 tslice nflux.out -o nflux1990to2000.txt -f 1990 -t 2000 -lon 1 -lat 2 -y 3
-aslice nflux1961to1990.txt -o nflux1961to1990_areaaverage.txt -lon 1 -lat 2 -n -pixsize 0.5 0.5
+aslice nflux1961to1990.txt -o nflux1961to1990_areaaverage.txt -lon 1 -lat 2 -n -pixsize 0.5 0.5 -pixoffset 0.0 0.0
 describe_textfile nflux1961to1990_areaaverage.txt "Global Terrestrial Nitrogen Fluxes, 1961 to 1990. Units: kgN ha-1"
-aslice nflux1990to2000.txt -o nflux1990to2000_areaaverage.txt -lon 1 -lat 2 -n -pixsize 0.5 0.5
+aslice nflux1990to2000.txt -o nflux1990to2000_areaaverage.txt -lon 1 -lat 2 -n -pixsize 0.5 0.5 -pixoffset 0.0 0.0
 describe_textfile nflux1990to2000_areaaverage.txt "Global Terrestrial Nitrogen Fluxes, 1990 to 2000. Units: kgN ha-1"
 
-aslice cpool1961to1990.txt -o cpool1961to1990_areaaverage.txt -lon 1 -lat 2 -n -pixsize 0.5 0.5
+aslice cpool1961to1990.txt -o cpool1961to1990_areaaverage.txt -lon 1 -lat 2 -n -pixsize 0.5 0.5 -pixoffset 0.0 0.0
 describe_textfile cpool1961to1990_areaaverage.txt "Global Terrestrial Carbon Pools, 1961 to 1990. Units: kgC m-2"
 
-aslice npool1961to1990.txt -o npool1961to1990_areaaverage.txt -lon 1 -lat 2 -n -pixsize 0.5 0.5
+aslice npool1961to1990.txt -o npool1961to1990_areaaverage.txt -lon 1 -lat 2 -n -pixsize 0.5 0.5 -pixoffset 0.0 0.0
 describe_textfile npool1961to1990_areaaverage.txt "Global Terrestrial Nitrogen Pools, 1961 to 1990. Units: kgN m-2"
 
-aslice tot_runoff1961to1990.txt -o tot_runoff1961to1990_areaaverage.txt -lon 1 -lat 2 -n -pixsize 0.5 0.5
+aslice tot_runoff1961to1990.txt -o tot_runoff1961to1990_areaaverage.txt -lon 1 -lat 2 -n -pixsize 0.5 0.5 -pixoffset 0.0 0.0
 describe_textfile tot_runoff1961to1990_areaaverage.txt "Global Runoff, 1961 to 1990. Units: mm yr-1"
 
 gmap lai1961to1990max.txt -t 'Dominant PFT (greatest LAI)' -lon 1 -lat 2 -i 3 -legend legend_global.txt -portrait -o maxLAI.jpg
