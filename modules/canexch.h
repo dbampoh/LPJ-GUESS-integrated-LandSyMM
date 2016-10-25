7421aac0a5b33d8f9b519e394964b6b845a7702c
Johan Nord <johan.nord@nateko.lu.se>
2016-10-25 15:15:20 +0000

    Retune model: changed alphaa constants downwards by around 15%. Decision pending review of output.
    If decision negative, revert change!
    
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/cruncepv7@5369 70805675-57b6-444b-bb00-18ecffcb4be8

--- modules/canexch.h
+++ modules/canexch.h
@@ -56,17 +56,17 @@ const double BC3 = 0.015;
 const double BC4 = 0.02;
 
 const double CMASS = 12.0;		// atomic mass of carbon
-const double ALPHAA = 0.55;		// value chosen to give global carbon pool and flux values that
+const double ALPHAA = 0.45;		// value chosen to give global carbon pool and flux values that
 								// agree with published estimates.
 								// scaling factor for PAR absorption from leaf to plant projective area level
 								// alias "twigloss". Should normally be in the range 0-1
 
-const double ALPHAA_NLIM = 0.7; // Same as ALPHAA above but chosen to give pools and flux values
+const double ALPHAA_NLIM = 0.6; // Same as ALPHAA above but chosen to give pools and flux values
 								// that agree with published estimates when Nitrogen limitation is
 								// switched on.
 
-const double ALPHAA_CROP = 0.75;		// Value for crops without N limitation.
-const double ALPHAA_CROP_NLIM = 1.0;	// Value for crops with N limitation
+const double ALPHAA_CROP = 0.65;		// Value for crops without N limitation.
+const double ALPHAA_CROP_NLIM = 0.85;	// Value for crops with N limitation
 
 /// Lambert-Beer extinction law (Prentice et al 1993; Monsi & Saeki 1953)
 inline double lambertbeer(double lai) {
