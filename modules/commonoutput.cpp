61d73e28c28ad3104851185adc6bcbb262fa5ca4
Niklas Boke Olén <niklasolen@gmail.com>
2017-06-13 08:52:47 +0000

    NOT FINISHED
    * update daily NEE output calculation to include repr
    
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/daily_grass@6054 70805675-57b6-444b-bb00-18ecffcb4be8

--- modules/commonoutput.cpp
+++ modules/commonoutput.cpp
@@ -1680,8 +1680,8 @@ void CommonOutput::outdaily(Gridcell& gridcell) {
 		outlimit(out,out_dflux, flux_fire);
 		outlimit(out,out_dflux, flux_est);
 
-		// daily NEE do not include fire, establishment and repr
-		outlimit(out,out_dflux, flux_veg  + flux_soil + flux_seed + flux_charvest);
+		// daily NEE do not include fire, establishment
+		outlimit(out,out_dflux, flux_veg   + flux_repr +  flux_soil + flux_seed + flux_charvest);
 
 
 	} // end if date year > spinup year
