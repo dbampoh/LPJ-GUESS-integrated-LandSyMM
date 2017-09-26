42f3ea310b6e760b9dc699054a3a732c3b7ed572
Michael Misurov <mischa@nateko.lu.se>
2017-09-26 16:33:12 +0000

    meta data file needs special treatment
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/daily_grass@6329 70805675-57b6-444b-bb00-18ecffcb4be8

--- benchmarks/fluxnet/preprocess.sh
+++ benchmarks/fluxnet/preprocess.sh
@@ -4,6 +4,9 @@ set -e
 declare -A fields
 fields=([TIMESTAMP]=0 [TA_ERA]=0 [SW_IN_ERA]=0 [P_ERA]=0 [NEE_CUT_MEAN]=0 [GPP_NT_VUT_REF]=0)
 
+iconv -f latin1 -t utf-8 < *_sitelist.csv |
+    awk -vFPAT='[^,]*|"[^"]*"' -vOFS='\t' '$1 !~ /^$/ { print $7,$6,$1}' > fixed.txt
+
 for f in *.zip; do
     fname=$(basename "$f")
     location=${fname:4:6}
@@ -16,13 +19,13 @@ for f in *.zip; do
         fi
         i=$(( $i+1 ))
     done
-    grep $location *_sitelist.csv | awk -vFPAT='[^,]*|"[^"]*"' -vOFS='\t' '{print $7,$6,$1}' |
-        tee $location.csv |
-        awk -vOFS='\t' 'function norm(x, y){y=int(x*2); return(2*x<y?y-1:y)/2+.25} {print norm($1), norm($2), $3}' >> gridlist.txt
+    grep $location fixed.txt | tee $location.csv |
+        awk -vOFS='\t' 'function norm(x, y){y=int(x*2); return(2*x<y?y-1:y)/2+.25}
+            {print norm($1), norm($2), $3}' >> gridlist.txt
     grep -vP '^.{4}0229' "$fname" | cut -d, -f$(echo ${fields[@]} | tr ' ' ,) --output-delimiter=$'\t' |
         sed -r '1d;s/.{4}/&\t/' > tmp
     cut -f-5 tmp >> $location.csv
     cut -f6- tmp | sed "s/^/$location\t/" >> validation.csv
     rm $fname
 done
-rm tmp
+rm tmp fixed.txt
