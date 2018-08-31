f0d782a9ceee3e73531eb287ebfd82b211442fd8
Johan Nord <johan.nord@nateko.lu.se>
2018-08-31 09:09:07 +0000

    Moved code of postprocess_agb.sh to postprocess.sh of crop_global benchmark
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/fire_blaze_merge@6920 70805675-57b6-444b-bb00-18ecffcb4be8

--- benchmarks/crop_global/postprocess.sh
+++ benchmarks/crop_global/postprocess.sh
@@ -17,6 +17,17 @@ function prepareyielddata {
 	awk -v c="$c" ' BEGIN { FS = " " } ; FNR==NR{a[$1$2]=$c;next}BEGIN{OFS=" "};{if($1$2 in a){print $3,a[$1$2]*1.3}}' $1 $2 > $3
 }
 
+# get above ground biomass data
+function prepare_agb {
+	model_input=$1
+	output=$2
+	dvar=$3
+	c=$(head -1 $model_input | awk -v d=$dvar '{for(i=1;i<=NF;i++){if($i==d){print i;}}}')
+	#awk -v c="$c" ' BEGIN { FS = " " } ; FNR==NR{a[$1$2]=$c;next}BEGIN{OFS=" "};{if($1$2 in a){print $3,a[$1$2]*0.7}}' $1 >> $2
+	awk -v c="$c" '{print $1,$2,$c}' $model_input > $output
+}
+# Liu AGB 
+
 describe_benchmark "LPJ-GUESS - Global Benchmarks for crops"
 source scatter_plot.sh
 common1961to1990.sh
@@ -62,3 +73,20 @@ describe_image maize_yield.png "Modelled compared to SPAM data set. Units: kg m-
 prepareyielddata yield1996to2005.txt common/../crop_global/spam_yield_wheat.dat temp_wheat.dat TeWW
 scatter_plot "Wheat yields" "SPAM" "LPJ-GUESS" temp_wheat.dat wheat_yield.png
 describe_image wheat_yield.png "Modelled compared to SPAM data set. Units: kg m-2." embed
+
+# Above Ground Biomass
+
+tslice cpool.out -f 1993 -t 2012 -o cpool1993-2012.dat
+prepare_agb cpool1993-2012.dat cpool1993-2012_agb.dat VegC
+joyn cpool1993-2012_agb.dat Global_mean_ABC_1993-2012_Liu2015_SI.dat -i Lon Lat -fast -o cpool1993-2012_joyned.dat
+
+# delta plot Liu cpool VegC
+ 
+awk '{if(FNR==1){print $1,$2, $3} else {print $1,$2, $4}}' cpool1993-2012_joyned.dat > cpool1993-2012_joyned_Liu.dat
+awk '{print $1,$2, $3}' cpool1993-2012_joyned.dat > cpool1993-2012_joyned_VegC.dat
+delta cpool1993-2012_joyned_VegC.dat cpool1993-2012_joyned_Liu.dat -i Lon Lat -o delta_cpool1993-2012_joyned.dat
+
+# Scatterplot Liu cpool VegC
+awk '(FNR>1){print $3, $4}' cpool1993-2012_joyned.dat > scat_cpool.dat
+scatter_plot "AGB" "Liu et al. " "LPJ-GUESS" scat_cpool.dat agb.png
+describe_image agb.png "Modelled compared to Liu et al. data. Units: kg m-2." embed
