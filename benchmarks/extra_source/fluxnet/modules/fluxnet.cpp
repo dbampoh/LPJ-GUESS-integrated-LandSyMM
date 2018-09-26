7c2b87c8cb7f748d5e7cd7e592c201c4f4234e45
Adrian Gustafson <adrian.gustafson@nateko.lu.se>
2018-09-26 16:45:30 +0000

    added bias correction of data in input.
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/benchmark_fluxnet@6964 70805675-57b6-444b-bb00-18ecffcb4be8

--- benchmarks/extra_source/fluxnet/modules/fluxnet.cpp
+++ benchmarks/extra_source/fluxnet/modules/fluxnet.cpp
@@ -7,112 +7,379 @@
 ///
 ///////////////////////////////////////////////////////////////////////////////////////
 
-#include "fluxnet.h"
+#include "cruinput.h"
+#include "guess.h"
 #include "driver.h"
 #include "guessstring.h"
+#include "fluxnet.h"
 #include <fstream>
 #include <sstream>
 
 namespace {
-    xtring file_cru;
-    xtring file_cru_misc;
+
+	xtring file_cru;
+	xtring file_cru_misc;
+
 }
 
-
+REGISTER_INPUT_MODULE("fluxnet", FluxnetInput)
 void FluxnetInput::init() {
-    CRUInput::init();
-    
-    file_cru=param["file_cru"].str;
-    file_cru_misc=param["file_cru_misc"].str;
-}
+	CRUInput::init(); 
 
-bool FluxnetInput::get_fluxnet_data_from_file() {
-	
-	std::string line;
-	
-	tair.clear();
-	rain.clear();
-	swrad.clear();
-	
-	xtring fluxfile = param["flux_dir"].str + gridlist.getobj().descrip.printable() + ".csv";
-	std::ifstream ifs(fluxfile, std::ifstream::in);
+	file_cru = param["file_cru"].str;
+	file_cru_misc = param["file_cru_misc"].str;
 
-	if (!ifs.good()) {
-		dprintf("FluxnetInput::getgridcell: could not open %s for input\n", (char*)fluxfile);
-		return false;
+	for (int i = 0; i < 12; i++) {
+		monthly_fluxnet_rad[i] = 0.0;
+		monthly_fluxnet_rain[i] = 0.0;
+		monthly_fluxnet_temp[i] = 0.0;
 	}
-	
-	while (getline(ifs, line)) {
-		
-		std::string monthday;
-		double file_temp, file_prec, file_insol;
-		int year;
-		
-		std::istringstream iss(line);
-		if (iss >> year >> monthday >> file_temp >> file_insol >> file_prec) {
-			if (tair.empty()) {
-				first_fluxnet_year = year;
-			}
+};
 
-			tair.push_back(file_temp);
-			swrad.push_back(file_insol);
-			rain.push_back(file_prec);
+void FluxnetInput::adjust_raw_forcing_data(double hist_mtemp[NYEAR_HIST][12],
+			double hist_mprec[NYEAR_HIST][12], double hist_msun[NYEAR_HIST][12],
+			double fluxnet_temp[12], double fluxnet_prec[12], double fluxnet_sun[12]) {
+	
+	double cru_temp_mean[12], cru_rain_mean[12], cru_rad_mean[12];
+	double cru_temp_anom[12], cru_rain_anom[12], cru_rad_anom[12];
+
+	for (int i = 0; i < 12; i++) {
+		cru_rad_mean[i] = 0.0;
+		cru_temp_mean[i] = 0.0;
+		cru_rain_mean[i] = 0.0;
+
+		cru_rad_anom[i] = 0.0;
+		cru_temp_anom[i] = 0.0;
+		cru_rain_anom[i] = 0.0;
+	}
+
+	for (int yr = first_year - FIRSTHISTYEAR; yr < last_year - FIRSTHISTYEAR; yr++) {
+		for (int m = 0; m < 12; m++) {
+			cru_temp_mean[m] += hist_mtemp[yr][m] / nyear;
+			cru_rad_mean[m] += hist_msun[yr][m] / nyear;
+			//cru_rain_mean[m] += hist_mprec[yr][m] / nyear;
+		};
+	};
+
+	for (int i = 0; i < 12; i++) {
+		cru_rad_anom[i] = cru_rad_mean[i] / fluxnet_sun[i];
+		cru_temp_anom[i] = cru_temp_mean[i] / fluxnet_temp[i];
+		//cru_rain_anom[i] = cru_rain_mean[i] / fluxnet_prec[i];
+	}
+
+	for (int yr = 0; yr < NYEAR_HIST; yr++) {
+		for (int m = 0; m < 12; m++) {
+			hist_mtemp[yr][m] *= cru_temp_anom[m];
+			//hist_mprec[yr][m] *= cru_rain_anom[m];
+			hist_msun[yr][m] *= cru_rad_anom[m];
 		}
 	}
-	
-	ifs.close();
-	
-	std::vector<double>::size_type days = rain.size();
-	if (days % 365) {
-		dprintf("Given time series doesn't extend for a full number of years (length: %d)\n", days);
-		return false;
-	}
-	return true;
-}
+
+
+};
 
 bool FluxnetInput::getgridcell(Gridcell& gridcell) {
-	if (!CRUInput::getgridcell(gridcell)){
-		return false;
-	}
 	
-	return FluxnetInput::get_fluxnet_data_from_file();
+	// See base class for documentation about this function's responsibilities
 
+	int soilcode;
+	int elevation;
+
+	// Make sure we use the first gridcell in the first call to this function,
+	// and then step through the gridlist in subsequent calls.
+	if (first_call) {
+		gridlist.firstobj();
+
+		// Note that first_call is static, so this assignment is remembered
+		// across function calls.
+		first_call = false;
+	}
+	else gridlist.nextobj();
+
+	if (gridlist.isobj) {
+
+		bool gridfound = false;
+		bool LUerror = false;
+		double lon;
+		double lat;
+
+		while (!gridfound) {
+
+			if (gridlist.isobj) {
+
+				lon = gridlist.getobj().lon;
+				lat = gridlist.getobj().lat;
+				gridfound = CRU_TS30::findnearestCRUdata(searchradius, file_cru, lon, lat, soilcode,
+					hist_mtemp, hist_mprec, hist_msun);
+
+				if (gridfound) // Get more historical CRU data for this grid cell
+					gridfound = CRU_TS30::searchcru_misc(file_cru_misc, lon, lat, elevation,
+						hist_mfrs, hist_mwet, hist_mdtr);
+
+				if (run_landcover && gridfound) {
+					LUerror = landcover_input.loadlandcover(lon, lat);
+					if (!LUerror)
+						LUerror = management_input.loadmanagement(lon, lat);
+				}
+
+				if (!gridfound || LUerror) {
+					if (!gridfound)
+						dprintf("\nError: could not find stand at (%g,%g) in climate data files\n", gridlist.getobj().lon, gridlist.getobj().lat);
+					else if (LUerror)
+						dprintf("\nError: could not find stand at (%g,%g) in landcover/management data file(s)\n", gridlist.getobj().lon, gridlist.getobj().lat);
+					gridfound = false;
+					gridlist.nextobj();
+				}
+			}
+			else return false;
+		}
+
+		///FLUXNET UNIQUE CODE:
+
+		tair.clear();
+		rain.clear();
+		swrad.clear();
+
+		// Fetch the fluxnetdata from the gridlist description
+		xtring fluxfile = param["flux_dir"].str + gridlist.getobj().descrip + ".csv";
+		std::ifstream ifs(fluxfile, std::ifstream::in);
+
+		if (!ifs.good()) {
+			dprintf("FluxnetInput::getgridcell: could not open %s for input\n", (char*)fluxfile);
+			return false;
+		}
+
+		std::string line;
+		std::istringstream iss(line);
+
+		while (getline(ifs, line)) {
+
+			int month;
+			double temp, prec, rad;
+
+			std::istringstream iss(line);
+
+			if (iss >> last_year >> month >> temp >> rad >> prec) {
+				if (tair.empty()) {
+					dailyoutput_firstyear = first_year = last_year;
+				}
+				tair.push_back(temp);
+				swrad.push_back(rad);
+				rain.push_back(prec);
+			}
+		}
+
+		ifs.close();
+
+		// Check so that input contains full years
+		std::vector<double>::size_type days = rain.size();
+		if (days % 12) {
+			dprintf("Given time series doesn't extend for a full number of years (length: %d)\n", days);
+			return false;
+		}
+
+		nyear = last_year - first_year;
+
+		// Make monthly averages from the input data
+		int idx = 0;
+		for (int i = 0; i < tair.size(); i++) {
+			monthly_fluxnet_temp[idx] += tair[i] / nyear;
+			monthly_fluxnet_rad[idx] += swrad[i] / nyear;
+			monthly_fluxnet_rain[idx] += rain[i] / nyear;
+			idx++;
+			if (idx % 12 == 0) { idx = 0; }
+		}
+
+		// Give sub-classes a chance to modify the data
+		adjust_raw_forcing_data(hist_mtemp, hist_mprec, hist_msun, monthly_fluxnet_temp, 
+			monthly_fluxnet_rain, monthly_fluxnet_rad);
+
+		// Build spinup data sets
+		spinup_mtemp.get_data_from(hist_mtemp);
+		spinup_mprec.get_data_from(hist_mprec);
+		spinup_msun.get_data_from(hist_msun);
+
+		// Detrend spinup temperature data
+		spinup_mtemp.detrend_data();
+
+		// guess2008 - new spinup data sets
+		spinup_mfrs.get_data_from(hist_mfrs);
+		spinup_mwet.get_data_from(hist_mwet);
+		spinup_mdtr.get_data_from(hist_mdtr);
+
+		// We wont detrend dtr for now. Partly because dtr is at the moment only
+		// used for BVOC, so what happens during the spinup is not affecting
+		// results in the period thereafter, and partly because the detrending
+		// can give negative dtr values.
+		//spinup_mdtr.detrend_data();
+
+
+		dprintf("\nCommencing simulation for stand at (%g,%g)", gridlist.getobj().lon,
+			gridlist.getobj().lat);
+		if (gridlist.getobj().descrip != "") dprintf(" (%s)\n\n",
+			(char*)gridlist.getobj().descrip);
+		else dprintf("\n\n");
+
+		// Tell framework the coordinates of this grid cell
+		gridcell.set_coordinates(gridlist.getobj().lon, gridlist.getobj().lat);
+
+		// Get nitrogen deposition data. 
+		/* Since the historic data set does not reach decade 2010-2019,
+		* we need to use the RCP data for the last decade. */
+		ndep.getndep(param["file_ndep"].str, lon, lat, Lamarque::RCP60);
+
+		// The insolation data will be sent (in function getclimate, below)
+		// as incoming shortwave radiation, averages are over 24 hours
+
+		gridcell.climate.instype = SWRAD_TS;
+
+		// Tell framework the soil type of this grid cell
+		soilparameters(gridcell.soiltype, soilcode);
+
+		// For Windows shell - clear graphical output
+		// (ignored on other platforms)
+
+		clear_all_graphs();
+
+		return true; // simulate this stand
+	}
+
+	return false; // no more stands
+	
 }
-
-    
-    
 
 bool FluxnetInput::getclimate(Gridcell& gridcell) {
 	
 	if (!CRUInput::getclimate(gridcell)) {
 		return false;
-	};
-	
-	int year = date.get_calendar_year();
-	if (year >= first_fluxnet_year) {
-		// Overwrite/extend climate data with site values for the period
-		int id = (year - first_fluxnet_year) * 365 + date.day;
-
-		
-		if (date.day == 0) {
-			// Put this years daily precipitation into an array
-			int ct=0;
-			for (int i=id; i < id+365; i++) {
-				drain[ct] = rain[i];
-				ct++;
-			}
-			double mndrydep[12], mnwetdep[12];
-			ndep.get_one_calendar_year(year, mndrydep, mnwetdep);
-			distribute_ndep(mndrydep, mnwetdep, drain, dndep_fluxnet);
-		}
-		
-		gridcell.climate.prec = rain[id];
-		gridcell.climate.temp = tair[id];
-		gridcell.climate.insol = swrad[id];
-		gridcell.climate.dndep = dndep_fluxnet[date.day];
 	}
 	return true;
 }
+/*
+bool FluxnetInput::getclimate(Gridcell& gridcell) {
+	double progress;
 
+	Climate& climate = gridcell.climate;
 
-REGISTER_INPUT_MODULE("fluxnet", FluxnetInput)
+	if (date.day == 0) {
+
+		// First day of year ...
+
+		// Extract N deposition to use for this year,
+		// monthly means to be distributed into daily values further down
+		double mndrydep[12], mnwetdep[12];
+		ndep.get_one_calendar_year(date.year - nyear_spinup + FIRSTHISTYEAR,
+			mndrydep, mnwetdep);
+
+		if (date.year < nyear_spinup) {
+
+			// During spinup period
+
+			if (date.year == state_year && restart) {
+
+				int year_offset = state_year % NYEAR_SPINUP_DATA;
+
+				for (int y = 0; y<year_offset; y++) {
+					spinup_mtemp.nextyear();
+					spinup_mprec.nextyear();
+					spinup_msun.nextyear();
+					spinup_mfrs.nextyear();
+					spinup_mwet.nextyear();
+					spinup_mdtr.nextyear();
+				}
+			}
+
+			int m;
+			double mtemp[12], mprec[12], msun[12];
+			double mfrs[12], mwet[12], mdtr[12];
+
+			for (m = 0; m<12; m++) {
+				mtemp[m] = spinup_mtemp[m];
+				mprec[m] = spinup_mprec[m];
+				msun[m] = spinup_msun[m];
+
+				mfrs[m] = spinup_mfrs[m];
+				mwet[m] = spinup_mwet[m];
+				mdtr[m] = spinup_mdtr[m];
+			}
+
+			// Interpolate monthly spinup data to quasi-daily values
+			interp_climate(mtemp1, mprec, msun, mdtr, dtemp, dprec, dsun, ddtr);
+
+			// Only recalculate precipitation values using weather generator
+			// if rainonwetdaysonly is true. Otherwise we assume that it rains a little every day.
+			if (ifrainonwetdaysonly) {
+				// (from Dieter Gerten 021121)
+				prdaily(mprec, dprec, mwet, gridcell.seed);
+			}
+
+			spinup_mtemp.nextyear();
+			spinup_mprec.nextyear();
+			spinup_msun.nextyear();
+
+			spinup_mfrs.nextyear();
+			spinup_mwet.nextyear();
+			spinup_mdtr.nextyear();
+
+		}
+		else if (date.year < nyear_spinup + NYEAR_HIST) {
+
+			// Historical period
+
+			// Interpolate this year's monthly data to quasi-daily values
+			interp_climate(hist_mtemp[date.year - nyear_spinup],
+				hist_mprec[date.year - nyear_spinup], hist_msun[date.year - nyear_spinup],
+				hist_mdtr[date.year - nyear_spinup],
+				dtemp, dprec, dsun, ddtr);
+
+			// Only recalculate precipitation values using weather generator
+			// if ifrainonwetdaysonly is true. Otherwise we assume that it rains a little every day.
+			if (ifrainonwetdaysonly) {
+				// (from Dieter Gerten 021121)
+				prdaily(hist_mprec[date.year - nyear_spinup], dprec, hist_mwet[date.year - nyear_spinup], gridcell.seed);
+			}
+		}
+		else {
+			// Return false if last year was the last for the simulation
+			return false;
+		}
+
+		// Distribute N deposition
+		distribute_ndep(mndrydep, mnwetdep, dprec, dndep);
+	}
+
+	// Send environmental values for today to framework
+
+	climate.co2 = co2[FIRSTHISTYEAR + date.year - nyear_spinup];
+
+	climate.temp = dtemp[date.day];
+	climate.prec = dprec[date.day];
+	climate.insol = dsun[date.day];
+
+	// Nitrogen deposition
+	climate.dndep = dndep[date.day];
+
+	// bvoc
+	if (ifbvoc) {
+		climate.dtr = ddtr[date.day];
+	}
+
+	// First day of year only ...
+
+	if (date.day == 0) {
+
+		// Progress report to user and update timer
+
+		if (tmute.getprogress() >= 1.0) {
+			progress = (double)(gridlist.getobj().id*(nyear_spinup + NYEAR_HIST)
+				+ date.year) / (double)(gridlist.nobj*(nyear_spinup + NYEAR_HIST));
+			tprogress.setprogress(progress);
+			dprintf("%3d%% complete, %s elapsed, %s remaining\n", (int)(progress*100.0),
+				tprogress.elapsed.str, tprogress.remaining.str);
+			tmute.settimer(MUTESEC);
+		}
+	}
+
+	return true;
+}
+*/
--- benchmarks/extra_source/fluxnet/modules/fluxnet.h
+++ benchmarks/extra_source/fluxnet/modules/fluxnet.h
@@ -22,11 +22,20 @@ public:
     bool getclimate(Gridcell& gridcell);
 	
 	bool get_fluxnet_data_from_file();
+
+	void adjust_raw_forcing_data(double hist_mtemp[NYEAR_HIST][12], 
+								 double hist_mprec[NYEAR_HIST][12],
+								 double hist_msun[NYEAR_HIST][12], 
+								 double fluxnet_temp[12],
+								 double fluxnet_prec[12],
+								 double fluxnet_sun[12]);
     
 private:
 	std::vector<double> rain, tair, swrad;
-	int first_fluxnet_year;
+	int first_year, last_year, nyear;
 	
+	double monthly_fluxnet_temp[12], monthly_fluxnet_rain[12], monthly_fluxnet_rad[12];
+
 	// Daily precipitation during one year. Needed for the N-deposition.
 	double drain[Date::MAX_YEAR_LENGTH];
 	// Daily nitrogen deposition distributed with fluxnet precipitation
