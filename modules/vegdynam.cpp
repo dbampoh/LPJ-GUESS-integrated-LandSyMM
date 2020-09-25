d5c1b5bcda74da907b0d4d97185de77d402fe7d4
David Wårlind <david.warlind@nateko.lu.se>
2020-09-25 15:18:31 +0000

    Updated the litterfall scheme
    Evergreens and grass drops 1/days_of_year of previous years litter every day
    Summergreens drop 1/31 of previous years litter every day for Jan in NH and Jul in SH
    Raingreens drop 1/day_of_month of previous years litter on previous years month with lowest average TrBR phen (month with strongest water limitation).
    Crops keep their day of litterfall.
    
    Updated some names of litter variables to more standard naming
    
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/litterfall@8904 70805675-57b6-444b-bb00-18ecffcb4be8

--- modules/vegdynam.cpp
+++ modules/vegdynam.cpp
@@ -1338,8 +1338,8 @@ void fire(Patch& patch,double& fireprob) {
 	// Loop through PFTs
 
 	for (p=0;p<npft;p++) {
-		litter_ag += patch.pft[p].litter_leaf + patch.pft[p].litter_sap + patch.pft[p].litter_heart +
-			patch.pft[p].litter_repr;
+		litter_ag += patch.pft[p].cmass_litter_leaf + patch.pft[p].cmass_litter_sap + patch.pft[p].cmass_litter_heart +
+			patch.pft[p].cmass_litter_repr;
 	}
 
 	// Soil Litter
@@ -1360,8 +1360,8 @@ void fire(Patch& patch,double& fireprob) {
 	// Loop through PFTs
 
 	for (p=0;p<npft;p++) {
-		me_mean += (patch.pft[p].litter_leaf + patch.pft[p].litter_sap + patch.pft[p].litter_heart +
-			patch.pft[p].litter_repr) * patch.pft[p].pft.litterme / litter_ag;
+		me_mean += (patch.pft[p].cmass_litter_leaf + patch.pft[p].cmass_litter_sap + patch.pft[p].cmass_litter_heart +
+			patch.pft[p].cmass_litter_repr) * patch.pft[p].pft.litterme / litter_ag;
 	}
 
 	// Soil litter
@@ -1415,18 +1415,18 @@ void fire(Patch& patch,double& fireprob) {
 		// Calculate flux from burnt litter
 
 		patch.fluxes.report_flux(Fluxes::FIREC,
-		                         mort_fire*(patch.pft[p].litter_leaf + patch.pft[p].litter_sap +
-		                                    patch.pft[p].litter_heart + patch.pft[p].litter_repr));
+		                         mort_fire*(patch.pft[p].cmass_litter_leaf + patch.pft[p].cmass_litter_sap +
+		                                    patch.pft[p].cmass_litter_heart + patch.pft[p].cmass_litter_repr));
 
 		report_fire_nfluxes(patch, mort_fire * (patch.pft[p].nmass_litter_leaf +
 			                patch.pft[p].nmass_litter_sap + patch.pft[p].nmass_litter_heart));
 
 		// Account for burnt above ground litter
 
-		patch.pft[p].litter_leaf        *= (1.0 - mort_fire);
-		patch.pft[p].litter_sap         *= (1.0 - mort_fire);
-		patch.pft[p].litter_heart       *= (1.0 - mort_fire);
-		patch.pft[p].litter_repr        *= (1.0 - mort_fire);
+		patch.pft[p].cmass_litter_leaf  *= (1.0 - mort_fire);
+		patch.pft[p].cmass_litter_sap   *= (1.0 - mort_fire);
+		patch.pft[p].cmass_litter_heart *= (1.0 - mort_fire);
+		patch.pft[p].cmass_litter_repr  *= (1.0 - mort_fire);
 		patch.pft[p].nmass_litter_leaf  *= (1.0 - mort_fire);
 		patch.pft[p].nmass_litter_sap   *= (1.0 - mort_fire);
 		patch.pft[p].nmass_litter_heart *= (1.0 - mort_fire);
