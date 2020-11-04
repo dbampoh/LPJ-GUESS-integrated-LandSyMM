eaa487e888cf5677801faa7c9788c75b8bb6f86e
Johan Nord <johan.nord@nateko.lu.se>
2020-11-04 13:47:22 +0000

    Removed ampersand in panarctic/postprocess.sh. Ampersand in describe commands causes the xml parser to crash.
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/arctic_retuning@9142 70805675-57b6-444b-bb00-18ecffcb4be8

--- benchmarks/panarctic/postprocess.sh
+++ benchmarks/panarctic/postprocess.sh
@@ -82,7 +82,7 @@ describe_images "Panarctic - Total runoff (1961-90 average)" total_runoff.jpg
 tslice fpc.out -f 2005 -t 2015 -lon 1 -lat 2 -y 3 -o fpc2005to2015.txt
 compute fpc2005to2015.txt -i 'TreeFPC=BNE+BINE+BNS+IBS+TeBS' -o treefpc.txt
 gmap treefpc.txt -p MERCATOR -s 0 0.01 10 -i TreeFPC -horiz -o treefpc.jpg
-describe_images "Panarctic - Tree FPC & treeline (2005-2015 average)" treefpc.jpg
+describe_images "Panarctic - Tree FPC and treeline (2005-2015 average)" treefpc.jpg
 
 
 # Cleanup
