436b23ec223438594abff9b9e9173cf7e043c859
Mats Lindeskog <mats.lindeskog@nateko.lu.se>
2021-01-15 18:39:44 +0000

    Removing BLAZE fix that will go into the bugfix branch separately (again).
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/forestry_4.1.2@9411 70805675-57b6-444b-bb00-18ecffcb4be8

--- modules/blaze.cpp
+++ modules/blaze.cpp
@@ -1021,7 +1021,6 @@ void blaze_accounting_gridcell(Climate& climate) {
 			gridcell.monthly_burned_area[i] = 0.0;
 		}
 	}
-	gridcell.can_burn = 0;
 
 	if ( is_first_day ) {
 
@@ -1193,7 +1192,9 @@ void blaze_driver(Patch& patch, Climate& climate) {
 		if ( gridcell.can_burn > 0 ) {
 			gridcell.annual_burned_area              += gridcell.burned_area;
 			gridcell.monthly_burned_area[date.month] += gridcell.burned_area;
+			gridcell.can_burn = 0;
 		}
+		gridcell.burned_area = 0.0;
 	}
 }
 
