66616792f3dfa2204fd3d4a18629eadbba292fcb
Johan Nord <johan.nord@nateko.lu.se>
2021-12-03 14:08:11 +0000

    Can now start >1 post-pp job. All other To-Dos from the the previous commit remains
    Works but needs som cleanup. 
    And needs me to add the directories and the script in postpost/delta:
    * On simba i added locally these to make it work. 
    
    Changes
    * Can now start >1 post-pp job.
      It now works to use benchmarks -p -r "delta xxx" -r "tellme xxx" <outp-path> to start the script postpost/delta/postpostprocess.sh
    * Postprocess.sh now called postpostprocess.sh: reason: scripts postprocess.sh are run by runpostprocess.
    * Svn-added the folderstructure
    
    Status
    * runpostpostprocess is still missing; postpostprocess.sh is still called directly by script benchmarks
      vilket leder till följande:
      * The pwd that postprocess.sh experiences is = <outp-path>
        This should be changed somehow - maybe by a common script runpostpostprocess in postpost/
        ... but how do i sent the ${superreport_arglist[@]} via runpostpostprocess to postprocess.sh?
      * slurmlog hamnar i <outp-path> istället för i delta
    
    Todo:
    * nu outputtas vissa saker som inte skall outputtas till terminalen
    * qsub
    * (rm commonscript)S
    * ev mer att göra helt klart.
    * slurmlog skall läggas i "delta" istället för i <outp-path>
    
    
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/benchmark_tellme@10271 70805675-57b6-444b-bb00-18ecffcb4be8

---
+++ benchmarks/postpost/delta/postpostprocess.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Delta tool" #>/tmp/johan.212102.ppp-delta
+echo "Arguments = $@" #>>/tmp/johan.212102.ppp-delta
+pwd -P #>>/tmp/johan.212102.ppp-delta
---
+++ benchmarks/postpost/tellme/postpostprocess.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Tellme tool" #>/tmp/johan.212102.ppp-delta
+echo "Arguments = $@" #>>/tmp/johan.212102.ppp-delta
+pwd -P #>>/tmp/johan.212102.ppp-delta
