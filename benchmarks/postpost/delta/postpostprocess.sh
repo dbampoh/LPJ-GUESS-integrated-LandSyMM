12d46a2573ad4b1c39942875a5ec5b1959c8b0ba
Johan Nord <johan.nord@nateko.lu.se>
2022-02-11 12:02:18 +0000

    Comment-message: Decision to rename postpostprocess-files and folders to exoprocess.
    Decision by Johan and Stefan 2022-02-11 
    to rename postpostprocess-files and folders to exoprocess.
    because postpostprocess is impossible to pronounce.
    exo meaning "on the outside" etc.
    e.g.:
         benchmarks\postpost\delta\postpostprocess.sh"
     --> benchmarks\exoprocesses\delta\exoprocess.sh"
    
    ToDo: to implement this decided change of naming.
    
    Other ToDos: see previous commit message.
    
    
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/benchmark_tellme@10404 70805675-57b6-444b-bb00-18ecffcb4be8

--- benchmarks/postpost/delta/postpostprocess.sh
+++ benchmarks/postpost/delta/postpostprocess.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Decision by Johan and Stefan 2022-02-11 
+# to rename postpostprocess-files and folders to exoprocess.
+# because postpostprocess is impossible to pronounce.
+# exo meaning "on the outside" etc.
+# e.g.:
+#      benchmarks\postpost\delta\postpostprocess.sh"
+#  --> benchmarks\exoprocesses\delta\exoprocess.sh"
+#					^					^
+
 echo "Delta tool" | tee postpostprocess.dummy-output
 echo "Arguments = $@" | tee -a postpostprocess.dummy-output
 echo "Argument 1 = $1" | tee -a postpostprocess.dummy-output
