f74506423199c333a06b3a6c5a8246ab5d704916
Mats Lindeskog <mats.lindeskog@nateko.lu.se>
2022-02-22 16:32:29 +0000

    Removing creation of directory for st_output on Linux.
    To avoid append complications.
    The files will be with the standard output files.
    
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/forestry_4.1.2@10423 70805675-57b6-444b-bb00-18ecffcb4be8

--- modules/miscoutput.cpp
+++ modules/miscoutput.cpp
@@ -672,11 +672,9 @@ void MiscOutput::define_output_tables() {
 
 	if(printstandtypes) {
 		char dirname[200]={'\0'};
-		strcpy(dirname, "st_output/");
 #ifdef _MSC_VER
+		strcpy(dirname, "st_output/");
 		_mkdir(dirname);
-#else
-		mkdir(dirname, 0777);
 #endif
 		out_cmass_pft_st = new Table[nst];
 		out_diamstruct_cmass_st = new Table[nst];
