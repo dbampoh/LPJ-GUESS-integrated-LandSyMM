cf415a38700ec58ba1e535c7d43339a5d22fd87a
Mats Lindeskog <mats.lindeskog@nateko.lu.se>
2022-10-06 14:40:14 +0000

    Removing unnecessary warning.
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/forestry_4.1.2@11087 70805675-57b6-444b-bb00-18ecffcb4be8

--- framework/externalinput.cpp
+++ framework/externalinput.cpp
@@ -234,7 +234,7 @@ void LandcoverInput::init() {
 					all_fracs_const = false;				//Set all_fracs_const to false if yearly data
 
 				// Avoid large number of output files
-				if(LUdata.GetNCells() > 100) {
+				if(printseparatestands && LUdata.GetNCells() > 100) {
 					printseparatestands = false;
 					dprintf("\nStand output disabled for simulations with land cover input files with more than 100 gridcells to avoid a large number of output files\nLandcover input file has data for %d gridcells\n", LUdata.GetNCells());
 				}
