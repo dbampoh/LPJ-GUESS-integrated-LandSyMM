56fbb793a026cad2385da5a3bc2eab36892414b8
Johan Nord <johan.nord@nateko.lu.se>
2023-04-03 16:18:32 +0000

    Updated the exoprocesses/tellme/exoprocess.sh script according to instructions from Matt email 2023-03-13.
    TODO: The script needs to be updated with code for determining the path to the Rscript tellme.Rmd 
    which will probably, I guess, be sitting in "svnhead"/benchmarks/exoprocesses/tellme/
    I e. the code needs to figure out, or get as a call argument, the path to the LPJG code folder.
    
    Also, see r10728 for a list of TODOs.
    
    
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/benchmark_tellme@11712 70805675-57b6-444b-bb00-18ecffcb4be8

--- benchmarks/exoprocesses/tellme/exoprocess.sh
+++ benchmarks/exoprocesses/tellme/exoprocess.sh
@@ -1,8 +1,49 @@
 #!/bin/bash
 
-echo "Tellme tool" | tee postpostprocess.dummy-output
-echo "Arguments = $@" | tee -a postpostprocess.dummy-output
-echo "Argument 1 = $1" | tee -a postpostprocess.dummy-output
-echo "Argument 2 = $2" | tee -a postpostprocess.dummy-output
-echo "pwd -P = $(pwd -P)" | tee -a postpostprocess.dummy-output
+# This script is called with 3 arguments:
+# $1:	Name-tag for the comparison reference benchmark
+# $2:	Name-tag for the new output
+# $3:	Path to the comparison reference benchmark output
 
+# The call to the Rscript tellme.Rmd here should have the folllowing syntax (email from Matt 2023-03-13)
+# (3 of the 6 arguments are generated within this script).
+# tellme.Rmd <path-to-new-benchmarking-runs> <new-name> <path-to-old-run-benchmarking-runs> <old-name> 
+# <path-to-benchrmarking-data-dir> <path-to-land-use-data>
+
+# These paths are compute center specific. Update to fit the center where you run your model.
+DATA_PATH=/data/benchmark_data/
+LUDATA_PATH=/data/benchmark_data/2019-04-29/landuse/
+RSCRIPT_PATHFILE="/.../.../tellme.Rmd"	# This is probably wrong. I bet tellme.Rmd is sitting in exoprocesses/tellme/
+					# and if so it's path needs to be provided as a call argument $4 to this script.
+
+DEBUG_LOG_NAME="exoprocess.debug.log"
+
+NAMETAG_NEW="$1"
+NAMETAG_REF="$2"
+REF_OUTPUT_PATH="$3"
+
+NEW_OUTPUT_PATH="$(dirname $(pwd -P))"
+
+
+if [ $# -ne 3 ]; then
+  echo "Error: the exoprocesses/tellme/exoprocess.sh script must be called with exctly 3 arguments." | tee $DEBUG_LOG_NAME
+  echo "The call arguments were: $@"
+  exit 1
+fi
+
+
+echo "Tellme tool" | tee $DEBUG_LOG_NAME
+echo "./benchmarks arguments = $@"   | tee -a $DEBUG_LOG_NAME
+echo "Arguments to the tellus R-script:" | tee -a $DEBUG_LOG_NAME
+echo "<path-to-new-benchmarking-runs> <new-name> <path-to-old-run-benchmarking-runs> <old-name> <path-to-benchrmarking-data-dir> <path-to-land-use-data>" | tee -a $DEBUG_LOG_NAME
+echo "Argument 1 = $NEW_OUTPUT_PATH" | tee -a $DEBUG_LOG_NAME
+echo "Argument 2 = $NAMETAG_NEW"     | tee -a $DEBUG_LOG_NAME
+echo "Argument 3 = $REF_OUTPUT_PATH" | tee -a $DEBUG_LOG_NAME
+echo "Argument 4 = $NAMETAG_REF"     | tee -a $DEBUG_LOG_NAME
+echo "Argument 5 = $DATA_PATH"       | tee -a $DEBUG_LOG_NAME
+echo "Argument 6 = $LUDATA_PATH"     | tee -a $DEBUG_LOG_NAME
+echo "'$0' = $0"            | tee -a $DEBUG_LOG_NAME
+echo "pwd -P = $(pwd -P)"   | tee -a $DEBUG_LOG_NAME
+
+# Here the call to the Rscript tellme.Rmd
+#$RSCRIPT_PATHFILE $NEW_OUTPUT_PATH $NAMETAG_NEW $REF_OUTPUT_PATH $NAMETAG_REF $DATA_PATH $LUDATA_PATH 
