49ac030d73a7611628fba8ded2a1e6307112a058
Johan Nord <johan.nord@nateko.lu.se>
2023-04-06 06:13:10 +0000

    Changed the path to evaluation data. Also the name of the corresponding variable. Adjusted cmts.
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/benchmark_tellme@11740 70805675-57b6-444b-bb00-18ecffcb4be8

--- benchmarks/exoprocesses/tellme/exoprocess.sh
+++ benchmarks/exoprocesses/tellme/exoprocess.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 # This script is called with 3 arguments:
-# $1:	Name-tag for the new output
-# $2:	Name-tag for the comparison reference benchmark
-# $3:	Path to the comparison reference benchmark output
-
+#   $1:	Name-tag for the new output
+#   $2:	Name-tag for the comparison reference benchmark
+#   $3:	Path to the comparison reference benchmark output
 # The call to the Rscript tellme.Rmd here should have the folllowing syntax (email from Matt 2023-03-13)
 # (3 of the 6 arguments are generated within this script).
-# tellme.Rmd <path-to-new-benchmarking-runs> <new-name> <path-to-old-run-benchmarking-runs> <old-name> 
-# <path-to-benchrmarking-data-dir> <path-to-land-use-data>
+#   tellme.Rmd <path to new benchmarking runs> <new name> <path to old run benchmarking runs> <old name> 
+#   <path to evaluation data> <path to land use data>
 
-# These paths are compute center specific. Update to fit the center where you run your model.
-DATA_PATH=/data/benchmark_data/
+# These paths are compute center specific. Update them to fit the center where you run your model.
+EVALDATA_PATH=/data/evaluation_data/
 LUDATA_PATH=/data/benchmark_data/2019-04-29/landuse/
 
 
@@ -35,15 +34,15 @@ fi
 echo "Tellme tool" | tee $DEBUG_LOG_NAME
 echo "./benchmarks arguments = $@"   | tee -a $DEBUG_LOG_NAME
 echo "Arguments to the tellus R-script:" | tee -a $DEBUG_LOG_NAME
-echo "<path-to-new-benchmarking-runs> <new-name> <path-to-old-run-benchmarking-runs> <old-name> <path-to-benchrmarking-data-dir> <path-to-land-use-data>" | tee -a $DEBUG_LOG_NAME
+echo "<path to new benchmarking runs> <new name> <path to old run benchmarking runs> <old name> <path to evaluation data> <path to land use data>" | tee -a $DEBUG_LOG_NAME
 echo "Argument 1 = $NEW_OUTPUT_PATH" | tee -a $DEBUG_LOG_NAME
 echo "Argument 2 = $NAMETAG_NEW"     | tee -a $DEBUG_LOG_NAME
 echo "Argument 3 = $REF_OUTPUT_PATH" | tee -a $DEBUG_LOG_NAME
 echo "Argument 4 = $NAMETAG_REF"     | tee -a $DEBUG_LOG_NAME
-echo "Argument 5 = $DATA_PATH"       | tee -a $DEBUG_LOG_NAME
+echo "Argument 5 = $EVALDATA_PATH"       | tee -a $DEBUG_LOG_NAME
 echo "Argument 6 = $LUDATA_PATH"     | tee -a $DEBUG_LOG_NAME
 echo "'$0' = $0"      | tee -a $DEBUG_LOG_NAME
 echo "pwd = $(pwd)"   | tee -a $DEBUG_LOG_NAME
 
 # Here the call to the Rscript tellme.Rmd
-$RSCRIPT_PATHFILE $NEW_OUTPUT_PATH $NAMETAG_NEW $REF_OUTPUT_PATH $NAMETAG_REF $DATA_PATH $LUDATA_PATH
+$RSCRIPT_PATHFILE $NEW_OUTPUT_PATH $NAMETAG_NEW $REF_OUTPUT_PATH $NAMETAG_REF $EVALDATA_PATH $LUDATA_PATH
