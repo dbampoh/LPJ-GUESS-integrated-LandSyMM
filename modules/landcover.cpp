02fc7545fad7f09f56b45d8b9dd8e988fea6d803
Johan Nord <johan.nord@nateko.lu.se>
2023-04-11 14:48:58 +0000

    Re-instated fix r11769: the fix of a text-bug in landcover.cpp.
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/tech_license@11772 70805675-57b6-444b-bb00-18ecffcb4be8

--- modules/landcover.cpp
+++ modules/landcover.cpp
@@ -2,8 +2,6 @@
 /// \file landcover.cpp
 /// \brief Functions handling landcover aspects, such as creating or resizing Stands
 ///
-/// Landcover change.
-///
 /// \author Mats Lindeskog,
 /// \Part of code in this file as well as in cropphenology.cpp, cropallocation.cpp and
 /// \management.cpp based on LPJ-mL C++ code received from Alberte Bondeau in 2008.
