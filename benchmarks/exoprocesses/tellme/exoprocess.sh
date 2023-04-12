7cd05972d1b1af8673a326854fba59d2034e9830
Johan Nord <johan.nord@nateko.lu.se>
2023-04-12 16:46:23 +0000

    Change of data paths from lunarc paths into simba paths. Plus commenting and cleanup of outcommented code.
    A note for the previous commit regading the source of tellme.Rmd:
    It was emailed to me from Matt today.
    
    
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/benchmark_tellme@11776 70805675-57b6-444b-bb00-18ecffcb4be8

--- benchmarks/exoprocesses/tellme/exoprocess.sh
+++ benchmarks/exoprocesses/tellme/exoprocess.sh
@@ -10,11 +10,11 @@
 #   <path to evaluation data> <path to land use data>
 
 # These paths are compute center specific. Update them to fit the center where you run your model.
-EVALDATA_PATH=/lunarc/nobackup/projects/snic2020-6-23/lpjguess/data/evaluation_data
-LUDATA_PATH=/lunarc/nobackup/projects/snic2020-6-23/lpjguess/data/benchmark_data/2023_03_02/landuse/LUH2/lu_1901_2015_luh2_Hist_CMIP_UofMD_landState_2_1_h_halfdeg_nourban_2019_11_15.txt
+EVALDATA_PATH=/data/evaluation_data
+LUDATA_PATH=/data/benchmark_data/2023_03_02/landuse/LUH2/lu_1901_2015_luh2_Hist_CMIP_UofMD_landState_2_1_h_halfdeg_nourban_2019_11_15.txt
 
 
-RSCRIPT="tellme.Rmd"
+RSCRIPT="tellme.Rmd"	# Do not change, unless you change also further down in this script where tellme.Rmd is hardcoded.
 DEBUG_LOG_NAME="exoprocess.debug.log"
 
 NAMETAG_NEW="$1"
@@ -27,10 +27,12 @@ RSCRIPT_PATHFILE="$(dirname $0)/${RSCRIPT}"
 if [ $# -ne 3 ]; then
   echo "Error: the exoprocesses/tellme/exoprocess.sh script must be called with exctly 3 arguments." | tee $DEBUG_LOG_NAME
   echo "The call arguments were: $@"
+  echo "You can rerun the tellme/exoprocess.sh without reruning the model run(s). How-to: run ./benchmarks without arguments"
+  echo "to get a help message that explains how to run the tellme/exoprocess.sh without reruning the model."
   exit 1
 fi
 
-
+}
 echo "Tellme tool" | tee $DEBUG_LOG_NAME
 echo "./benchmarks arguments = $@"   | tee -a $DEBUG_LOG_NAME
 echo "Arguments to the tellus R-script:" | tee -a $DEBUG_LOG_NAME
@@ -43,13 +45,10 @@ echo "Argument 5 = $EVALDATA_PATH"       | tee -a $DEBUG_LOG_NAME
 echo "Argument 6 = $LUDATA_PATH"     | tee -a $DEBUG_LOG_NAME
 echo "'$0' = $0"      | tee -a $DEBUG_LOG_NAME
 echo "pwd = $(pwd)"   | tee -a $DEBUG_LOG_NAME
+} >/dev/null	# Don't output debug info to the console / slurm.log.
 
-
-set -x
+set -x		# Debug. Remove later.
 
 # Here the call to the Rscript tellme.Rmd
 cp $RSCRIPT_PATHFILE .
 Rscript -e "rmarkdown::render('tellme.Rmd',params=list(new_directory=\"$NEW_OUTPUT_PATH\",new_name=\"$NAMETAG_NEW\",old_directory=\"$REF_OUTPUT_PATH\",old_name=\"$NAMETAG_REF\",data_directory=\"$EVALDATA_PATH\",land_cover_file=\"$LUDATA_PATH\"))"
-
-# Here the call to the Rscript tellme.Rmd
-#$RSCRIPT_PATHFILE $NEW_OUTPUT_PATH $NAMETAG_NEW $REF_OUTPUT_PATH $NAMETAG_REF $EVALDATA_PATH $LUDATA_PATH
