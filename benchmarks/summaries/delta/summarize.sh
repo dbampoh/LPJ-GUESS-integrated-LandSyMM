f2fd7e60c5bca917a822698bd1ea10b6cb4b31bf
Johan Nord <johan.nord@nateko.lu.se>
2025-03-12 15:46:07 +0000

    Implemented option switch -c in delta/summarize.sh so that it now works as a job submitted to a cluster node. Also removed old outcommented code. Todo...
    Todo:
    * If there is output to the top folder, then implement the same fixes for log-file output as done today with diff/summarize.sh.
      so that that output ends up in ./delta/ instead of in ./
    
    
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/benchmark_tellme@13432 70805675-57b6-444b-bb00-18ecffcb4be8

--- benchmarks/summaries/delta/summarize.sh
+++ benchmarks/summaries/delta/summarize.sh
@@ -8,26 +8,11 @@
 
 resultfile="deltareports.vs-${1}.result"
 
-echo "Exo-processing: Deltareports... Arguments = $@"
+echo "Summary-processing: Deltareports... Arguments = $@"
 echo "$0"
-echo "The exoscript's pwd -P = $(pwd -P)"
+echo "The summarize script's pwd -P = $(pwd -P)"
 cd ..
-deltareports $@ | tee -a delta/$resultfile
+#which deltareports | tee -a delta/$resultfile
+deltareports -c $@ | tee -a delta/$resultfile
 
 
-############## Old
-
-# Decision by Johan and Stefan 2022-02-11
-# to rename postpostprocess-files and folders to exoprocess.
-# because postpostprocess is impossible to pronounce.
-# exo meaning "on the outside" etc.
-# e.g.:
-#      benchmarks\postpost\delta\postpostprocess.sh"
-#  --> benchmarks\exoprocesses\delta\exoprocess.sh"
-#                  ^                  ^
-
-#echo "Delta tool" | tee postpostprocess.dummy-output
-#echo "Arguments = $@" | tee -a postpostprocess.dummy-output
-#echo "Argument 1 = $1" | tee -a postpostprocess.dummy-output
-#echo "Argument 2 = $2" | tee -a postpostprocess.dummy-output
-#echo "pwd -P = $(pwd -P)" | tee -a postpostprocess.dummy-output
