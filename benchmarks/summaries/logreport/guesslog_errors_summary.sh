5c34fa0716794920f7b84dfad6a6089a9e87264e
Johan Nord <johan.nord@nateko.lu.se>
2025-03-13 13:24:25 +0000

    Added guesslog_errors_summary.sh. Source is standalone version r968.
    git-svn-id: svn://stormbringer.nateko.lu.se/svn/LPJ-GUESS/branches/benchmark_tellme@13440 70805675-57b6-444b-bb00-18ecffcb4be8

---
+++ benchmarks/summaries/logreport/guesslog_errors_summary.sh
@@ -0,0 +1,173 @@
+#!/bin/bash
+# guess-grep-guesslog-errors-allBMs.sh
+# If no parameter, operates on current dir. Else operates on dir given in $1.
+# $1: dir containing bm folders with guess.log files.
+
+
+# Exclude BM(s?)
+EXCLUDEDBMS="fluxnet"
+#"tellus"
+
+# Exclude these words or strings, separate with |
+FILTERPATTERN="C pool change|C flux|Period C balance|C balance year|N pool change|N flux|Period N balance|N balance year"	# elapsed|^Commen|^Last|Using|C pool change|C fluxes|Period C|^$
+# and do it only on these BMs:
+FILTERBMS="tellus"
+
+
+if [ $# -eq 0 ]; then
+  outfile="`pwd -P;`/grep-guesslog-errors-allBMs.cout"
+
+else
+  outfile="`cd ; pwd -P;`/.grep-guesslog-errors-allBMs.tmp"
+  set -e
+  cd $1
+  set +e
+fi
+
+
+scriptpath="/home/jnord/bin/"
+
+expected_files_reference="trunk/output13034full"
+if [ "$ARCH" != "" ]; then
+  expected_files_reference="/lunarc/nobackup/projects/snic2020-6-23/jnord/benchmarks/${expected_files_reference}"
+else
+  expected_files_reference="/admin/data/lpjguess_benchmarks_output/trunk/outputRelease4.1_r10118full"  # On simba: not sam as above
+fi
+
+bms="crop_global crop_mixed_sites diurnal_pristine_sites emdi_europe emdi_global europe fluxnet global panarctic\
+ pristine_sites secondary_stands soil_temperature tellus wetland_global wetland_sites"
+
+
+
+### Main code
+echo "Please wait a few moments..."
+
+
+### Intro
+string_intro=$(
+#echo $0     #> $outfile
+echo "Analysis of:"
+pwd -P      #>> $outfile
+echo
+echo "This output is also saved in in file $outfile."
+echo
+echo "1a. Look for missing report/index.html files."
+echo "1b. Look for empty .out files."
+echo "2.  Print only unexpected output."
+echo "3.  Counting start- and finished-lines. The numbers should match."
+echo "4.  Check that number of outputfiles are ok (not missing), i.e.:"
+echo "    checking the presence of expected files or superfluous files as cmp w a BM reference."
+echo "5.  All output except expected warnings, i.e. start- and finished-lines, plus unexpected output."
+)
+
+
+### Verify that report-folders have been created
+string_missingreport=$(
+echo
+echo "_________________________________________________________"
+echo "Look for missing report/index.html. This should be empty."
+diff -u <(cd ${expected_files_reference};\
+ ls -d1 */report/index.html) <(ls -d1 */report/index.html)\
+ | grep -v ^\-\-\- | grep -v ^+++ | grep "^\-\|^+" | less
+echo
+)
+#} > $outfile 2>&1
+
+
+### Look for empty outfiles
+string_emptyoutfiles=$(
+echo
+echo "_______________________________________________________________________________________________"
+echo "Look for empty outfiles. This should be empty. This is not not a comparison with the reference."
+wc -l */*.out | awk '$1==0'
+echo
+)
+
+
+### Print guesslog errors and finished-lines from logs
+{	#string_guesslogerrors=$(
+echo
+for bmcat in $bms; do
+  if [[ $EXCLUDEDBMS =~ $bmcat ]]; then
+    echo "Excluded $bmcat"
+    echo
+    continue
+  fi
+  echo $bmcat
+  GENERAL="^(LPJ-GUESS cohort mode - |Using soil code and Nitrogen deposition for|LPJ-GUESS test Secondary stands|Description: |Last year of cropland fraction data used from year 2016 and onwards)"
+  if [[ $FILTERBMS =~ $bmcat ]]; then
+    FILTER="${GENERAL}|${FILTERPATTERN}"
+    echo "Extra excluded lines by these filter terms: $FILTERPATTERN"
+  else
+    FILTER="${GENERAL}"
+  fi
+  $scriptpath/guess-grep-guesslog-errors.sh $bmcat/guess.log \
+  | grep -vE "$FILTER"
+# note the backslash at the end of the prev line.
+  echo
+
+done
+
+} > $outfile 2>&1
+string_guesslogerrors=$(cat $outfile)
+
+
+string_unexpected=$(
+echo
+echo "______________________"
+echo "Unexpected output only"
+
+cat $outfile | grep -v Finished |grep -v "^\[LPJ-GUESS" \
+| grep -v ^"LPJ-GUESS cohort mode - " \
+| grep -v "^Using soil code and Nitrogen deposition for" \
+| grep -v "^LPJ-GUESS test Secondary stands" \
+| grep -v "^Description: " \
+| grep -v "^Last year of cropland fraction data used from year 2016 and onwards" 
+# note the backslash at the end onf the prev line.
+
+echo "________________________________________________"
+echo "Unexpected output plus Start- and Finished-lines" 
+
+nLPJG=$(cat $outfile | grep -c "\[LPJ-GUESS")
+nFinished=$(cat $outfile | grep -c "Finished")
+
+echo "Number of \[LPJ-GUESS found: $nLPJG"
+echo "Number of Finished found: $nFinished"
+echo
+echo "______________________________________________"
+echo "files as cmp w $expected_files_reference"
+echo "(diff of ls of each bm-catalog found in . with the reference):"
+
+for bmcat in $bms; do
+
+  if [ -d $bmcat ]; then
+    echo
+    echo $bmcat
+    diff -u <(ls "${expected_files_reference}/$bmcat") <(ls $bmcat) |\
+         grep -e "^\-\|^\+"  |\
+         grep -v slurm | grep -v diff_out | grep -v diff_t |\
+         grep -v "report_delta" | grep -v '\-\-\- /dev/fd/' | grep -v '\+\+\+ /dev/fd/'
+  fi
+done
+
+echo
+)
+#} >> $outfile 2>&1
+
+echo "$string_intro" > $outfile 2>&1
+echo >> $outfile 2>&1
+echo "$string_missingreport" >> $outfile 2>&1
+echo >> $outfile 2>&1
+echo "$string_emptyoutfiles" >> $outfile 2>&1
+echo >> $outfile 2>&1
+echo "$string_unexpected" >> $outfile 2>&1
+echo >> $outfile 2>&1
+echo "__________________________________________________" >> $outfile 2>&1
+echo "Print finished-lines from logs, and errors if any." >> $outfile 2>&1
+echo "$string_guesslogerrors" >> $outfile 2>&1
+
+echo
+echo Finshed
+echo The output was saved in $outfile
+
+less $outfile
